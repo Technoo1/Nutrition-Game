b1f87a64e9d9261be795ec56939379bca3deb71a
Ben-Lever <1026824@student.sae.edu.au>
2023-06-27 14:46:02 +0800

    Background Swapper cleaned up, Fungus dialogue placeholder made
    - press number keys to swap between placeholder scenes
    - Example dialogue with placeholder npc made
    - all fungus documentation and example scenes are in a folder (will probably delete at some point)

--- Assets/Scripts/BackgroundSwapper.cs
+++ Assets/Scripts/BackgroundSwapper.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class BackgroundSwapper : MonoBehaviour
 {
     public List<Sprite> backgroundImageList; //All backgrounds can be placed in here
     public Image image;
+    public float fadeDuration = 0.2f;
+    public int backgroundImageValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +19,66 @@ public class BackgroundSwapper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //press a number to change to the next background
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1) && image.sprite != backgroundImageList[0]) // set the value in "backgroundImageList[?] to the position of the image you're switching to
         {
-             image.sprite = backgroundImageList[0];
+            backgroundImageValue = 0; // change this to the position of the image you're switching to in the list
+            StartCoroutine(FadeToBlack()); // fades background to black, changes the image, then fades back
         }
-        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && image.sprite != backgroundImageList[1]) // set the value in "backgroundImageList[?] to the position of the image you're switching to
         {
-            image.sprite = backgroundImageList[1];
+            backgroundImageValue = 1; // change this to the position of the image you're switching to in the list
+            StartCoroutine(FadeToBlack()); // fades background to black, changes the image, then fades back
         }
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && image.sprite != backgroundImageList[2]) // set the value in "backgroundImageList[?] to the position of the image you're switching to
+        {
+            backgroundImageValue = 2; // change this to the position of the image you're switching to in the list
+            StartCoroutine(FadeToBlack()); // fades background to black, changes the image, then fades back
+        }
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        // fades the background to black
+        Color originalColor = image.color;
+        float timer = 0.0f;
+
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            float ratio = timer / fadeDuration;
+            image.color = Color.Lerp(originalColor, Color.black, ratio);
+            yield return null;
+        }
+
+        image.color = Color.black; // Ensure final color is black
+        image.sprite = backgroundImageList[backgroundImageValue]; // changes the background image
+        StartCoroutine(FadeBack()); // fades back to the new image
+    }
+
+    IEnumerator FadeBack()
+    {
+        Color originalColor = image.color;
+        float timer = 0.0f;
+
+        while (timer < fadeDuration)
+        {
+            timer += Time.deltaTime;
+            float ratio = timer / fadeDuration;
+            image.color = Color.Lerp(originalColor, Color.white, ratio);
+            yield return null;
+        }
+
+        image.color = Color.white;
+    }
+
+    public void GoToThisPlace() // called by the dialogue
+    {
+        backgroundImageValue = 1; // change this to the position of the image you're switching to in the list
+        StartCoroutine(FadeToBlack()); // fades background to black, changes the image, then fades back
+    }
+    public void GoToThatPlace() // called by the dialogue
+    {
+        backgroundImageValue = 2; // change this to the position of the image you're switching to in the list
+        StartCoroutine(FadeToBlack()); // fades background to black, changes the image, then fades back
     }
 }
