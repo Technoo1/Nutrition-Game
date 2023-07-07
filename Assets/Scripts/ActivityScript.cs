8556c6112c66e7ecdc1373110bccee984f09512f
Ben-Lever <1026824@student.sae.edu.au>
2023-07-07 14:26:40 +0800

    Made a game manager script, started player stats
    - made game manager script with player stats
    - made an activity script, with functions for "study" and "relax" activities
    -cleaned up the dialogue around "study" and "relax"
    - "relax" and "study" affect player stats accordingly
    - added sfx when the player "studies" or "relaxes"

---
+++ Assets/Scripts/ActivityScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+
+public class ActivityScript : MonoBehaviour
+{
+    public void Study()
+    {
+        GameManager.instance.academicAbilty += 10;
+        Debug.Log("+10 to academic ability");
+        Debug.Log("Your academic ability is: " + GameManager.instance.academicAbilty);
+    }
+    public void Relax()
+    {
+        GameManager.instance.stress -= 10;
+        GameManager.instance.energy += 10;
+        Debug.Log("-10 to stress");
+        Debug.Log("Your stress is " + GameManager.instance.stress);
+        Debug.Log("+10 to energy");
+        Debug.Log("Your energy is " + GameManager.instance.energy);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    // Add your variables here
+    public int health = 10;
+    public int stress = 10;
+    public int energy = 10;
+    public int academicAbilty = 10;
+    public float budget = 50.00f;
+    private void Awake()
+    {
+        // Ensure only one instance of the GameManager exists
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Add your methods and functionality here
+    public void Study()
+    {
+        GameManager.instance.academicAbilty += 10;
+        Debug.Log("+10 to academic ability");
+        Debug.Log("Your academic ability is: " + academicAbilty);
+    }
+    public void Relax()
+    {
+        GameManager.instance.stress -= 10;
+        GameManager.instance.energy += 10;
+        Debug.Log("-10 to stress");
+        Debug.Log("Your stress is " + stress);
+        Debug.Log("+10 to energy");
+        Debug.Log("Your energy is " + energy);
+    }
+}
